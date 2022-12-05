1eb61f7ade66eefb00c6eefbfdc0d275c840bd28
rifatptk <rifatptk@gmail.com>
2022-12-05 11:47:46 +0600

    small calendar header added

--- src/components/SmallCalendar.jsx
+++ src/components/SmallCalendar.jsx
@@ -39,6 +39,13 @@ const SmallCalendar = () => {
           </span>
         </button>
       </header>
+      <div className="grid grid-cols-7 grid-rows-6">
+        {currentMonth[0].map((day, dayIdx) => (
+          <span key={dayIdx} className="text-sm py-1 text-center">
+            {day.format('dd').charAt(0)}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
