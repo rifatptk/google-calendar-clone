9a7615956dd5fcd893adc6a5eb9844621998d62c
rifatptk <rifatptk@gmail.com>
2022-12-05 11:43:22 +0600

    add event button added

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -12,12 +12,17 @@ const CalendarHeader = () => {
   function handleNextMonth() {
     setMonthIndex(monthIndex + 1);
   }
+  function handleReset() {
+    setMonthIndex(dayjs().month());
+  }
   return (
     <header className="px-4 py-2 flex items-center">
       <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
 
-      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button className="border rounded py-2 px-4 mr-5" onClick={handleReset}>
+        Today
+      </button>
 
       <button onClick={handlePrevMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
@@ -30,7 +35,9 @@ const CalendarHeader = () => {
         </span>
       </button>
 
-      <h2>{dayjs(new Date(dayjs().year(), monthIndex)).format('MMMM YYYY')}</h2>
+      <h2 className="ml-4 text-xl text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format('MMMM YYYY')}
+      </h2>
     </header>
   );
 };
---
+++ src/components/SmallCalendar.jsx
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+import React, { useContext, useEffect, useState } from 'react';
+import GlobalContext from '../context/GlobalContext';
+import { getMonth } from '../utils';
+
+const SmallCalendar = () => {
+  const [currentMonthIdx, setcurrentMonthIdx] = useState(dayjs().month());
+  const [currentMonth, setcurrentMonth] = useState(getMonth());
+
+  useEffect(() => {
+    setcurrentMonth(getMonth(currentMonthIdx));
+  }, [currentMonthIdx]);
+
+  const { monthIndex } = useContext(GlobalContext);
+  useEffect(() => {
+    setcurrentMonthIdx(monthIndex);
+  }, [monthIndex]);
+
+  function handlePrevMonth() {
+    setcurrentMonthIdx(currentMonthIdx - 1);
+  }
+  function handleNextMonth() {
+    setcurrentMonthIdx(currentMonthIdx + 1);
+  }
+  return (
+    <div className="mt-9">
+      <header className="flex justify-between">
+        <p className="text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), currentMonthIdx)).format('MMMM YYYY')}
+        </p>
+        <button onClick={handlePrevMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+        </button>
+        <button onClick={handleNextMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default SmallCalendar;
